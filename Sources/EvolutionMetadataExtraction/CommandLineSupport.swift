3cb0c049ddd085ed29a15b31004c708869a76090
James Dempsey <dempsey@tapas-software.net>
2025-11-21 09:58:24 -0800

    Add declarations to support Linux - Add conditional imports of FoundationNetworking - Declare CF contants not present on Linux - Resolves #54

--- Sources/EvolutionMetadataExtraction/CommandLineSupport.swift
+++ Sources/EvolutionMetadataExtraction/CommandLineSupport.swift
@@ -10,6 +10,10 @@
 import Foundation
 import ArgumentParser
 
+#if canImport(FoundationNetworking)
+import FoundationNetworking // Required on Linux
+#endif
+
 nonisolated(unsafe) private var VERBOSE_ENABLED: Bool = false // Set once in `validate(verbose:)`, read once for `verboseEnabled`.
 func verbosePrint(_ items: Any..., additionalCondition: Bool = true, separator: String = " ", terminator: String = "\n\n") {
     if verboseEnabled && additionalCondition { print(items, separator: separator, terminator: terminator) }
