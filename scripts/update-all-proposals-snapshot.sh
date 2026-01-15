48a07979cfbbc6dbf0564d8b9fe970d07677e806
James Dempsey <dempsey@tapas-software.net>
2026-01-15 08:17:21 -0800

    Add utility scripts to update test files - Script to update all test files for tool/schema version changes - Script to update 'AllProposals' snapshot with latest set of proposals - Resolves #40

---
+++ scripts/update-all-proposals-snapshot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# update-all-proposals-snapshot
+#
+# The AllProposals snapshot should be updated periodically so that it contains
+# a recent snapshot of proposals in the swift-evolution repository.
+#
+# This script generates an updated AllProposals snapshot.
+#
+# The previous snapshot is moved to a 'ReplacedFiles' directory.
+#
+# The intended workflow is to inspect the changes of the newly generated snapshot,
+# ensure tests pass, commit the changes, and delete the ReplacedFiles directory and its contents.
+#
+
+script_dir=$(dirname "$(readlink -f "$0")")
+"$script_dir/update-test-files.sh" AllProposals
